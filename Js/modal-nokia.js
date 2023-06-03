38753984c87058ff5e46f1d7e87907ebb6230096
Lev <levthepredator@gmail.com>
2023-06-03 18:56:16 +0300

    sum-03.06.23

--- Js/modal-nokia.js
+++ Js/modal-nokia.js
@@ -4,12 +4,8 @@ const modalClose = document.querySelector('.modal-close');
 
 phoneBtn.addEventListener('click' , () => {
     html.style.overflow = 'hidden'
-})
+})  
 
 modalClose.addEventListener('click' , () => {
     html.style.overflow = ''
 })
-
-// document.querySelector('.modal-open-nokia').addEventListener('click' , () => {
-//     document.querySelector('.modal-nokia').classList.remove('modal-open-nokia')
-// })

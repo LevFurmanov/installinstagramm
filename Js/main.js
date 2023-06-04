e2c02ac419f5841e57a234d4ffee9e867d51ed60
Lev <levthepredator@gmail.com>
2023-06-04 23:53:10 +0300

    pravki2

--- Js/main.js
+++ Js/main.js
@@ -5,3 +5,15 @@ document.querySelector('.burger-button').addEventListener('click' , () => {
 document.querySelector('.modal__close').addEventListener('click' , () => {
     document.querySelector('.modal').classList.remove("modal_open")
 })
+
+document.querySelector('.modal__inme').addEventListener('click' , () => {
+    document.querySelector('.modal').classList.remove("modal_open")
+})
+
+document.querySelector('.modal__program').addEventListener('click' , () => {
+    document.querySelector('.modal').classList.remove("modal_open")
+})
+
+document.querySelector('.modal__retes').addEventListener('click' , () => {
+    document.querySelector('.modal').classList.remove("modal_open")
+})
